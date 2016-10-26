9a75c510dcd31c7ce56280c52c3d78003d5087cf
Davey <dwaite498@gmail.com>
2016-10-26 11:28:21 -0400

    assignment 2 change href to ui-sref

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,15 +7,19 @@
             });
         $stateProvider
             .state('landing', {
-            url: '/',
-            templateUrl: 'templates/landing.html'
-        })
+                url: '/',
+                templateUrl: 'templates/landing.html'
+            })
             .state('album', {
                 url: '/album',
                 templateUrl: '/templates/album.html'
-        });
+            })
+            .state('collection'. {
+                url: 'collection',
+                templateUrl: '/templates/collection.html'   
+            });
         
-    }
+        }
     angular
         .module('blocJams', [ui.router])
         .config(config);
