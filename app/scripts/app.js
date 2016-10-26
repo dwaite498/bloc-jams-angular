c50e47e4628cd58c8a065ef419dd6d3c8bef41db
Davey <dwaite498@gmail.com>
2016-10-26 11:23:35 -0400

    add $stateProvider functionality

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,6 +9,10 @@
             .state('landing', {
             url: '/',
             templateUrl: 'templates/landing.html'
+        })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
         });
         
     }
