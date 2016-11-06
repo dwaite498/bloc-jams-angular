dd62968bf40b664c29388b6c79e61f4405d43fd1
Davey <dwaite498@gmail.com>
2016-11-06 18:45:05 -0500

    working on assignment

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,45 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtURL: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: 161.71, audioURL: 'assets/music/blue' },
+        { title: 'Green', duration: 103.96, audioURL: 'assets/music/green' },
+        { title: 'Red', duration: 268.45, audioURL: 'assets/music/red' },
+        { title: 'Pink', duration: 153.14, audioURL: 'assets/music/pink' },
+        { title: 'Magenta', duration: 374.22, audioURL: 'assets/music/magenta' }
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtURL: 'assets/images/album_covers/20.png',
+    songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21' },
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15' }
+     ]
+};
+
+var albumWellington = {
+    title: 'The General',
+    artist: 'Arthur Wellesley',
+    label: 'Great Britain',
+    year: '1815',
+    albumArtURL: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Dublin Born', duration: '2:32' },
+        { title: 'Irish Soldier', duration: '3:01' },
+        { title: 'Peninsular Campaign', duration: '1:45' },
+        { title: 'Ambassador to France', duration: '1:00' },
+        { title: 'Waterloo Campaign', duration: '5:21' },
+        { title: 'Prime Minister Wellesley', duration: '3:22' }
+    ]
+};
