8e9e8b92d5e2745ef76037ec365c120de611e21a
Davey <dwaite498@gmail.com>
2016-11-15 17:54:47 -0500

    services part 2 checkpoint complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,14 +2,26 @@
     function SongPlayer() {
         var SongPlayer = {};
         
+        var currentSong = null;
+        var currentBuzzObject = null;
+        
         SongPlayer.play = function(song) {
-            var currentBuzzObject = new buz.sound(song.audioURL, {
+            if (currentSong !== song) {
+                if (currentBuzzObject) {
+                    currentBuzzObject.stop();
+                    currentSong.playing = null;
+                }
+            currentBuzzObject = new buz.sound(song.audioURL, {
                 formats: ['mp3'],
                 preload: true
             });
             
+            currentSong = song;
+            
             currentBuzzObject.play();
+            song.playing = true;
         }
+    };
         
         return SongPlayer;
     }
