bd15e46184012a30865c499e0170ff93a86c2995
Davey <dwaite498@gmail.com>
2016-11-19 16:00:57 -0500

    finish services part 2

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,10 +1,27 @@
 (function () {
     function SongPlayer() {
         var SongPlayer = {};
-        
+        /** @desc current song file 
+        *   @type {object}*/
         var currentSong = null;
+        /**
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
         var currentBuzzObject = null;
-        
+        /** @function playSong
+        *   @Desc plays the current song when called
+        *   @param {object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        }
+         /**
+         * @function setSong
+         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+         * @param {Object} song
+         */
         var setSong = function(song) {
             debugger;
             if (currentBuzzObject) {
@@ -19,13 +36,15 @@
  
             currentSong = song;
  };
-        
+        /** *function songplayer.play
+        *   @desc sets and plays a song when called
+        *   @param {object} song
+        */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
             setSong(song);
  
-            currentBuzzObject.play(); 
-            song.playing = true;
+            playSong(song);
                 
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
@@ -35,7 +54,10 @@
             
             
         };
-        
+        /** @function 
+        *   @desc pauses the song when called
+        *   @param {object} song
+        */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
