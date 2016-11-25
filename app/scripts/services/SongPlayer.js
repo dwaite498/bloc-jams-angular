e8d8518a0f1556a0fb205371269f3db2d51aa453
Davey <dwaite498@gmail.com>
2016-11-25 11:13:36 -0500

    add code from directives 2

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -47,6 +47,12 @@
                 formats: ['mp3'],
                 preload: true
             });
+            
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
+            })
  
             SongPlayer.currentSong = song;
         };
